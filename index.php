613fb6ed0b18c84f6953e06c9c356b299a13f42f
Codascii <dev.codascii@gmail.com>
2019-05-17 10:22:59 +0200

    Mise en place de sass

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" type="text/css" href="css/header.css"/>
-        <link rel="stylesheet" type="text/css" href="css/index.css"/>
+        <link rel="stylesheet" type="text/css" href="css/safewalk.css"/>
+        <!--<link rel="stylesheet" type="text/css" href="css/index.css"/>-->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous"/>
         <title>Safewalk</title>
@@ -26,9 +26,6 @@
                 <i class="fab fa-facebook-f"></i>
                 <span>SE CONNECTER AVEC FACEBOOK</span>
             </button>
-            <!--<form action="register.php" method="post">
-                <button id="register">S'INSCRIRE</button>
-            </form>-->
         </div>
         <script type="text/javascript" src="js/index.js"></script>
     </body>
